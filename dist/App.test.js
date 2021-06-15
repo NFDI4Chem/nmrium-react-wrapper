1b01666e469ced08c9367bea7437c9094321682f
Lan Le <baolan2005@gmail.com>
2021-06-15 13:57:06 +0200

    update babel to create package (#1)
    Co-authored-by: Lan Le <lan.le@kit.edu>

--- dist/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import NMRDisplayer from './lib/index';
-
-test('renders learn react link', () => {
-  // render(<div></div>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
