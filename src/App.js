c04595a3037937ada215a62925951f47604cdb89
Lan Le <lan.le@kit.edu>
2021-06-15 13:35:30 +0200

    update babel to create package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import React from 'react';
 import './App.css';
-import NMRium from 'nmrium';
+import NMRDisplayer from './lib/NMRDisplayer';
 
-function NMRDisplayer() {
-  return (
-    <div className="App">
-      <NMRium />
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jcampURL: ''
+    };
 
-export default NMRDisplayer;
+    this.receivePostMessage = this.receivePostMessage.bind(this);
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    if (match) {
+      const { jcamp } = match.params
+      this.setState({jcampURL: decodeURIComponent(jcamp)})
+    }
+
+    window.addEventListener("message", this.receivePostMessage)
+  }
+
+  receivePostMessage(event) {
+    if (event.data) {
+      this.setState({jcamp: event.data})
+    }
+  }
+
+  render() {
+    const { jcamp } = this.state;
+    return (
+      <div className="App">
+        <NMRDisplayer
+          jcamp={jcamp}/>
+      </div>
+    );
+  }
+};
