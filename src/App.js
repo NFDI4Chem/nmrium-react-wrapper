2f0cd26ceb9ce7327529e36d5275bb82204be5ba
Lan Le <lan.le@kit.edu>
2021-06-29 11:24:44 +0200

    update process jcamp data

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { jcampURL } = this.state;
+    let { jcampURL } = this.state;
     return (
       <div className="App">
         <NMRDisplayer
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NMRium from 'nmrium';
-import { addJcampFromURL } from 'nmrium/lib/data/SpectraManager';
+import { addJcampFromURL, addJcamp } from 'nmrium/lib/data/SpectraManager';
 
 export default class NMRDisplayer extends React.Component {
   constructor(props) {
@@ -11,24 +11,107 @@ export default class NMRDisplayer extends React.Component {
   }
 
   componentDidMount() {
-    this.loadJcamp();
+    // this.loadJcamp();
+    this.loadJcampURL();
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.jcampURL !== this.props.jcampURL) {
+      this.loadJcampURL();
+    }
+    if (prevProps.jcamp !== this.props.jcamp) {
       this.loadJcamp();
+    }
+    if (prevProps.jcampFile !== this.props.jcampFile) {
+      this.loadLocalJcampFile();
     }
   }
 
-  loadJcamp() {
+  loadJcampURL() {
     const { jcampURL } = this.props;
 
-    const spectra = []
-    
-    addJcampFromURL(spectra, jcampURL, {}).then(() => {
+    const spectra = [];
+
+    fetch('/api/v1/chemspectra/nmr_displayer/'+jcampURL, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json.jcamp)
+      addJcamp(spectra, json.jcamp, {})
       const spectraData = {spectra: spectra}
       this.setState({spectraData: spectraData})
-    });
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+
+  loadJcamp() {
+    const { jcamp } = this.props;
+    if (jcamp) {
+      const spectra = [];
+      addJcamp(spectra, jcamp, {})
+      const spectraData = {spectra: spectra}
+      this.setState({spectraData: spectraData})
+    }
+  }
+
+  fetchLocal(url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest
+        xhr.onload = function () {
+            resolve(new Response(xhr.response, { status: xhr.status }))
+        }
+        xhr.onerror = function () {
+            reject(new TypeError('Local request failed'))
+        }
+        xhr.open('GET', url)
+        xhr.responseType = "arraybuffer";
+        xhr.send(null)
+    })
+};
+
+  loadLocalJcampFile() {
+    const { jcampFile } = this.props;
+    if (jcampFile) {
+      console.log('jcamp file')
+      const spectra = [];
+      // console.log(jcampFile)
+      // this.fetchLocal('file:///home/eln/Documents/Working/chemotion_ELN/tmp/tmp_jcamp20210628-1776366-ifta9e')
+      // .then((response) => {
+      //   console.log(response)
+      // })
+      // .catch((err) => {
+      //   console.error(err);
+      // })
+      
+      // const ids = [parseInt(jcampFile)]
+      // console.log(ids)
+      // fetch('http://172.21.39.135:3000/api/v1/attachments/files/', {
+      //   method: 'POST',
+      //   mode: 'no-cors',
+      //   credentials: 'same-origin',
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({ ids }),
+      // }).then((response) => {
+      //   return response.json();
+      // }).then((json) => {
+      //   console.log(json)
+      //   return json;
+      // }).catch((error) => {
+      //   console.log("error")
+      //   console.log(error)
+      // })
+    }
   }
 
   render() {
