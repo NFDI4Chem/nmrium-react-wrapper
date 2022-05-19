0a405ffefa3d6c6702bc6861ac61654d8fec5c36
hamed-musallam <hamed.musallam@gmail.com>
2022-05-19 11:29:56 +0200

    test: load spectra from external URLs

---
+++ test-e2e/core.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+import NmriumWrapperPage from './NmriumWrapperPage';
+
+test('should load NMRium from external Urls', async ({ page }) => {
+  const nmrium = await NmriumWrapperPage.create(page);
+  expect(await nmrium.page.title()).toBe('NMRium Wrapper');
+
+  await nmrium.page.click('text=Test Load from URLS');
+
+  // if loaded successfully, there should be a 1H and 13C tabs
+  await expect(
+    nmrium.page.locator('_react=InternalTab[tabid = "1H"]'),
+  ).toBeVisible();
+  await expect(
+    nmrium.page.locator('_react=InternalTab[tabid = "13C"]'),
+  ).toBeVisible();
+});
