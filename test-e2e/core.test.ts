8aee00c4248ffe8af57900860703345c22361fcc
hamed-musallam <hamed.musallam@gmail.com>
2022-06-14 12:52:11 +0200

    test: load spectra from json object

--- test-e2e/core.test.ts
+++ test-e2e/core.test.ts
@@ -6,12 +6,23 @@ test('should load NMRium from external Urls', async ({ page }) => {
   const nmrium = await NmriumWrapperPage.create(page);
   expect(await nmrium.page.title()).toBe('NMRium Wrapper');
 
-  await nmrium.page.locator('text=Test Load from URLS').click();
+  await nmrium.page.click('text=Test Load from URLS');
 
   // if loaded successfully, there should be a 1H and 13C tabs
   await expect(
     nmrium.page.locator('_react=InternalTab[tabid = "1H"]'),
   ).toBeVisible();
+  await expect(
+    nmrium.page.locator('_react=InternalTab[tabid = "13C"]'),
+  ).toBeVisible();
+});
+
+test('should load NMRium from json', async ({ page }) => {
+  const nmrium = await NmriumWrapperPage.create(page);
+
+  await nmrium.page.click('text=Test load from json');
+
+  // if loaded successfully, there should be a 1H and 13C tabs
   await expect(
     nmrium.page.locator('_react=InternalTab[tabid = "13C"]'),
   ).toBeVisible();
