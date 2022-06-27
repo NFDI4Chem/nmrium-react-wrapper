5522bb46bd0c950afbb2170776c39017582d0f3c
Nisha Sharma <54287612+NishaSharma14@users.noreply.github.com>
2022-06-27 14:12:45 +0200

    Update event.ts
    Added test server domain.

--- src/events/event.ts
+++ src/events/event.ts
@@ -4,7 +4,7 @@ const ALLOWED_ORIGINS: string[] = [
   'https://nmrxiv.org',
   'http://localhost',
   'http://localhost:3000',
-  'http://test.nmrxiv.org'
+  'http://test.nmrxiv.org',
 ];
 
 const namespace = 'nmr-wrapper';
