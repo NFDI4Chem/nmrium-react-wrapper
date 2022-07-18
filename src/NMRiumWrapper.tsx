a80ca9bba15c534134d68045685d820a50c86a31
hamed-musallam <hamed.musallam@gmail.com>
2022-07-18 12:52:19 +0200

    refactor: use inline style

--- src/NMRiumWrapper.tsx
+++ src/NMRiumWrapper.tsx
@@ -1,32 +1,30 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
 import NMRium, { NMRiumData } from 'nmrium';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, CSSProperties } from 'react';
 import events from './events';
 import { usePreferences } from './hooks/usePreferences';
 import { useLoadSpectra } from './hooks/useLoadSpectra';
 
-const styles = {
-  container: css`
-    height: 100%;
-    width: 100%;
-  `,
+const styles: Record<'container' | 'loadingContainer', CSSProperties> = {
+  container: {
+    height: '100%',
+    width: '100%',
+  },
 
-  loadingContainer: css`
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #ffffffc9;
-    font-size: 1.4em;
-    user-select: none;
-    -webkit-user-drag: none;
-  `,
+  loadingContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffffc9',
+    fontSize: '1.4em',
+    userSelect: 'none',
+    WebkitUserSelect: 'none',
+  },
 };
 
 export type { NMRiumData };
@@ -73,9 +71,9 @@ export default function NMRiumWrapper() {
   });
 
   return (
-    <div css={styles.container}>
+    <div style={styles.container}>
       {isLoading && (
-        <div css={styles.loadingContainer}>
+        <div style={styles.loadingContainer}>
           <span>Loading .... </span>
         </div>
       )}
