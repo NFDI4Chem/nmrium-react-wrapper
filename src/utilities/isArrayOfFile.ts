653b98d45d9215fd665835eb894f9a719953dbd4
hamed-musallam <hamed.musallam@gmail.com>
2022-09-26 13:16:29 +0200

    refactor: clean code

--- src/utilities/isArrayOfFile.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function isArrayOfFile(data: any[]) {
-  return data.every(
-    (file) => file?.data !== undefined && file?.name !== undefined,
-  );
-}
--- src/utilities/isBruker.ts
+++
@@ -1,9 +0,0 @@
-import { JSZipObject } from 'jszip';
-
-type ZipFiles = { [key: string]: JSZipObject };
-
-export function isBruker(files: ZipFiles) {
-  return Object.keys(files).some((path) =>
-    ['2rr', 'fid', '1r'].some((brukerFile) => path.endsWith(brukerFile)),
-  );
-}
