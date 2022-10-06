8407f0d510115ae169ced14c2b0b6341425838a1
hamed-musallam <hamed.musallam@gmail.com>
2022-10-06 12:42:14 +0200

    feat: allow origins with IP addresses form

--- src/events/event.ts
+++ src/events/event.ts
@@ -1,10 +1,13 @@
 import { EventType, EventData } from './types';
+import { isIPAddress } from '../utilities/isIPAddress';
 
 const ALLOWED_ORIGINS: string[] = [
   'https://nmrxiv.org',
   'http://nmrxiv.org',
   'http://localhost',
   'http://localhost:3000',
+  'http://127.0.0.1:',
+  'http://127.0.0.1:3000',
   'http://test.nmrxiv.org',
   'http://193.196.39.168',
   'http://193.196.39.168:3000',
@@ -13,13 +16,17 @@ const ALLOWED_ORIGINS: string[] = [
 const namespace = 'nmr-wrapper';
 
 function parseOrigin(origin: string) {
+  let url: string | null = '';
   const urlSegments = origin.split('://');
-  const hostSegments = urlSegments[1].split('.');
-  const startIndex = hostSegments.length > 2 ? 1 : 0;
-
-  const url = `${urlSegments[0]}://${hostSegments
-    .slice(hostSegments.length > 1 ? startIndex : 0)
-    .join('.')}`;
+  if (isIPAddress(origin)) {
+    url = origin;
+  } else {
+    const hostSegments = urlSegments[1].split('.');
+    const startIndex = hostSegments.length > 2 ? 1 : 0;
+    url = `${urlSegments[0]}://${hostSegments
+      .slice(hostSegments.length > 1 ? startIndex : 0)
+      .join('.')}`;
+  }
 
   return url;
 }
