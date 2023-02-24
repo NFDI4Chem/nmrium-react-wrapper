63e8fe97cf02d1f097489bb6ce87a4638c3b35e3
hamed-musallam <hamed.musallam@gmail.com>
2023-02-24 11:07:19 +0100

    feat: update nmrium pre-release version 0.33.0-pre.1677156813

--- src/hooks/useLoadSpectra.ts
+++ src/hooks/useLoadSpectra.ts
@@ -1,51 +1,45 @@
 import { useCallback, useMemo, useState } from 'react';
-import { read, readSource, Source } from 'nmr-load-save';
-import { Spectrum } from 'nmr-load-save/lib/types/Spectra/Spectrum';
+import { read, readFromWebSource, NmriumState } from 'nmr-load-save';
 import { fileCollectionFromFiles } from 'filelist-utils';
 import events from '../events';
 import { isArrayOfString } from '../utilities/isArrayOfString';
 import { getFileNameFromURL } from '../utilities/getFileNameFromURL';
 
 async function loadSpectraFromFiles(files: File[]) {
-  // TODO use the new function from filelist-utils once they solve the problem of create filesCollection from files with empty webkitrelativepath
   const fileCollection = await fileCollectionFromFiles(files);
-  const result = await read(fileCollection);
+  const {
+    nmriumState: { data },
+  } = await read(fileCollection);
   // eslint-disable-next-line no-restricted-syntax
-  for (const spectrum of result.data.spectra) {
-    spectrum.source = {} as Source;
+  if (data) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const spectrum of data.spectra) {
+      spectrum.selector = {};
+    }
   }
-  return result;
+  return data;
 }
 
 async function loadSpectraFromURLs(urls: string[]) {
-  const promises = urls.map((url) => {
+  const entries = urls.map((url) => {
     const refURL = new URL(url);
     let name = getFileNameFromURL(url);
     const hasExtension = name && name.indexOf('.') !== -1;
     if (!hasExtension) {
       name = `${name}.zip`;
     }
-    return readSource({
-      baseURL: refURL.origin,
-      files: [{ relativePath: refURL.pathname, name }],
-    });
+    return { relativePath: refURL.pathname, baseURL: refURL.origin };
   }, []);
-  const results = await Promise.all(promises);
-  const spectra: any[] = [];
-  const molecules: any[] = [];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const result of results) {
-    spectra.push(...result.data.spectra);
-    molecules.push(...result.data.spectra);
-  }
-  return { spectra, molecules };
+
+  const { data } = await readFromWebSource({ entries });
+
+  return data;
 }
 
+type NMRiumData = NmriumState['data'];
+
 export function useLoadSpectra() {
-  const [data, setData] = useState<{
-    spectra: Spectrum[];
-    molecules: Record<string, string>[];
-  }>({ spectra: [], molecules: [] });
+  const [data, setData] = useState<NMRiumData>({ spectra: [], molecules: [] });
   const [isLoading, setLoading] = useState<boolean>(false);
 
   const load = useCallback(
@@ -54,19 +48,14 @@ export function useLoadSpectra() {
       try {
         if ('urls' in options) {
           if (isArrayOfString(options.urls)) {
-            const { spectra, molecules } = await loadSpectraFromURLs(
-              options.urls,
-            );
-            setData({ spectra, molecules });
+            const result = await loadSpectraFromURLs(options.urls);
+            setData(result as NMRiumData);
           } else {
             throw new Error('The input must be a valid urls array of string[]');
           }
         } else if ('files' in options) {
-          // TODO use the new function from filelist-utils once they solve the problem of create filesCollection from files with empty webkitrelativepath
-          const {
-            data: { spectra, molecules },
-          } = await loadSpectraFromFiles(options.files);
-          setData({ spectra, molecules });
+          const result = await loadSpectraFromFiles(options.files);
+          setData(result as NMRiumData);
         }
       } catch (error: any) {
         events.trigger('error', error);
