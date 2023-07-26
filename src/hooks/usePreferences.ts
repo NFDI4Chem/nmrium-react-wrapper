251fc843fbedbd77742752d1c8a204b88138e61b
hamed-musallam <hamed.musallam@gmail.com>
2023-07-26 12:05:38 +0200

    refactor: usePrefusePreferences hook

--- src/hooks/usePreferences.ts
+++ src/hooks/usePreferences.ts
@@ -1,26 +1,25 @@
-import { NMRiumPreferences, NMRiumWorkspace } from 'nmrium';
+import { WorkspacePreferences } from 'nmr-load-save';
+import { NMRiumWorkspace } from 'nmrium';
 import { useLayoutEffect, useState } from 'react';
 
 export function usePreferences() {
   const [configuration, setConfiguration] = useState<{
-    preferences: any;
+    preferences: WorkspacePreferences | undefined;
     workspace: NMRiumWorkspace | undefined;
-  }>({ preferences: {}, workspace: undefined });
+  }>({ preferences: undefined, workspace: undefined });
 
   useLayoutEffect(() => {
     const { href } = window.location;
     const parameters = new URL(href).searchParams;
 
-    let preferences: any | undefined;
+    let preferences: WorkspacePreferences | undefined;
     let workspace: NMRiumWorkspace | undefined;
 
     if (parameters.has('workspace')) {
       workspace = parameters.get('workspace') as NMRiumWorkspace;
     }
     if (parameters.has('preferences')) {
-      preferences = JSON.parse(
-        parameters.get('preferences') || '',
-      ) as NMRiumPreferences;
+      preferences = JSON.parse(parameters.get('preferences') || '');
     }
     setConfiguration({ preferences, workspace });
   }, []);
