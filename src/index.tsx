d1647f27217346a7c9d9c765ec07cc4f5b4c97b8
hamed musallam <hamed.musallam@gmail.com>
2023-12-14 12:25:46 +0100

    feat: update nmrium to version 0.46.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import 'react-science/styles/preflight.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 import { createRoot } from 'react-dom/client';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
